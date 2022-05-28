186f2bb1162a6ce66ee0f7a7fe40ea37ac818ddf
Uzama Zaid <uzamazaid@Uzamas-MacBook.local>
2022-05-28 20:04:35 +0530

    implemented server

--- http/server.go
+++ http/server.go
@@ -1,1 +1,30 @@
 package http
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+func StartServer(router *mux.Router) {
+	address := "0.0.0.0:8080"
+
+	server := &http.Server{
+		Addr:         address,
+		WriteTimeout: time.Second * 10,
+		ReadTimeout:  time.Second * 10,
+		IdleTimeout:  time.Second * 10,
+		Handler:      router,
+	}
+
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			log.Println(err.Error())
+		}
+	}()
+
+	log.Printf("server started at %s", address)
+}
