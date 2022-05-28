ca93af44433e75e62786b7cac423d99fd85e5a1d
Uzama Zaid <uzamazaid@Uzamas-MacBook.local>
2022-05-29 00:10:22 +0530

    added ibna informations

--- validator/validator.go
+++ validator/validator.go
@@ -14,8 +14,88 @@ type validator struct {
 }
 
 func NewValidator() Validator {
+
+	// source https://www.iban.com/structure
 	ibanLength := map[string]int{
+		"AL": 28, // Albania
+		"AD": 24, // Andorra
+		"AT": 20, // Austria
+		"AZ": 28, // Azerbaijan
+		"BH": 22, // Bahrain
+		"BY": 28, // Belarus
+		"BE": 16, // Belgium
+		"BA": 20, // Bosnia and Herzegovina
+		"BR": 29, // Brazil
+		"BG": 22, // Bulgaria
+		"CR": 22, // Costa Rica
+		"HR": 21, // Croatia
+		"CY": 28, // Cyprus
+		"CZ": 24, // Czech Republic
+		"DK": 18, // Denmark
+		"DO": 28, // Dominican Republic
+		"EG": 29, // Egypt
+		"SV": 28, // El Salvador
+		"EE": 20, // Estonia
+		"FO": 18, // Faroe Islands
+		"FI": 18, // Finland
+		"FR": 27, // France
+		"GE": 22, // Georgia
+		"DE": 22, // Germany
+		"GI": 23, // Gibraltar
+		"GR": 27, // Greece
+		"GL": 18, // Greenland
+		"GT": 28, // Guatemala
+		"VA": 22, // Holy See (the)
+		"HU": 28, // Hungary
+		"IS": 26, // Iceland
+		"IQ": 23, // Iraq
+		"IE": 22, // Ireland
+		"IL": 23, // Israel
+		"IT": 27, // Italy
+		"JO": 30, // Jordan
+		"KZ": 20, // Kazakhstan
+		"XK": 20, // Kosovo
+		"KW": 30, // Kuwait
+		"LV": 21, // Latvia
+		"LB": 28, // Lebanon
+		"LY": 25, // Libya
+		"LI": 21, // Liechtenstein
+		"LT": 20, // Lithuania
+		"LU": 20, // Luxembourg
+		"MT": 31, // Malta
+		"MR": 27, // Mauritania
+		"MU": 30, // Mauritius
+		"MD": 24, // Moldova
+		"MC": 27, // Monaco
+		"ME": 22, // Montenegro
+		"NL": 18, // Netherlands
+		"MK": 19, // North Macedonia
+		"NO": 15, // Norway
+		"PK": 24, // Pakistan
+		"PS": 29, // Palestine
+		"PL": 28, // Poland
+		"PT": 25, // Portugal
+		"QA": 29, // Qatar
+		"RO": 24, // Romania
+		"LC": 32, // Saint Lucia
+		"SM": 27, // San Marino
+		"ST": 25, // Sao Tome and Principe
+		"SA": 24, // Saudi Arabia
+		"RS": 22, // Serbia
+		"SC": 31, // Seychelles
+		"SK": 24, // Slovak Republic
+		"SI": 19, // Slovenia
+		"ES": 24, // Spain
+		"SD": 18, // Sudan
+		"SE": 24, // Sweden
+		"CH": 21, // Switzerland
+		"TL": 23, // Timor-Leste
+		"TN": 24, // Tunisia
+		"TR": 26, // Turkey
+		"UA": 29, // Ukraine
+		"AE": 23, // United Arab Emirates
 		"GB": 22, // United Kingdom
+		"VG": 24, // Virgin Islands, British
 	}
 
 	return &validator{
