25289791f557c2dcec20d22f8f466503e40e1acb
Uzama Zaid <uzamazaid@Uzamas-MacBook.local>
2022-05-28 20:06:53 +0530

    implemented router

--- http/router.go
+++ http/router.go
@@ -1,1 +1,15 @@
 package http
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func InitRouter() *mux.Router {
+	r := mux.NewRouter()
+
+	r.HandleFunc("validate", nil).Methods(http.MethodGet)
+
+	return r
+}
